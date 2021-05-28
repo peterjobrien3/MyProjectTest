047be11bfcc0f59a99a4e60a47530f387bf13725
peterjobrien3 <84069412+peterjobrien3@users.noreply.github.com>
2021-05-28 22:46:09 +0100

    Imported 3 CSV files and converted to Dataframe

--- main.py
+++ main.py
@@ -14,3 +14,14 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+import pandas as pd
+# Read population_by_country.csv
+population_by_country=pd.read_csv("population_by_country_2020.csv")
+print(population_by_country.head())
+print(population_by_country.info())
+GDP_by_country_WB=pd.read_csv("API_NY.GDP.MKTP.CD_DS2_en_csv_v2_2445719.csv",skiprows=4)
+print(GDP_by_country_WB.head())
+print(GDP_by_country_WB.info())
+Country_by_region_WB=pd.read_csv("Metadata_Country_API_NY.GDP.MKTP.CD_DS2_en_csv_v2_2445719.csv")
+print(Country_by_region_WB.head())
+print(Country_by_region_WB.info())
