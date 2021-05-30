729085e0d2e0702b5829aa447cd6fda087140e66
peterjobrien3 <84069412+peterjobrien3@users.noreply.github.com>
2021-05-30 22:23:51 +0100

    Finalised clean dat for GDP figures

--- main.py
+++ main.py
@@ -14,7 +14,10 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Install Pandas and Matplotlib
 import pandas as pd
+import matplotlib.pyplot as plt
+
 # Read population_by_country.csv
 population_by_country=pd.read_csv("population_by_country_2020.csv")
 print(population_by_country.head())
@@ -45,18 +48,30 @@ GDP_by_region_and_country_WB_merge_cl2.to_csv("merged_WB_v2.csv")
 GDP_by_region_and_country_WB_merge_cl3=GDP_by_region_and_country_WB_merge_cl2.rename({"Country Code_x":"Country Code"},axis=1)
 print(GDP_by_region_and_country_WB_merge_cl3.info())
 # Insert a column "Baseline_GDP" after Indicator Name with value 0 for all rows.
-
+GDP_by_region_and_country_WB_merge_cl3["Baseline_GDP"]=0
+print(GDP_by_region_and_country_WB_merge_cl3.head())
+# Move "Baseline_GDP" column position to after "Indicator Name" and before GBP data
+col_list = GDP_by_region_and_country_WB_merge_cl3.columns.tolist()
+col_list.insert(5, col_list.pop(col_list.index('Baseline_GDP')))
+print(col_list)
+GDP_by_region_and_country_WB_merge_cl3 = GDP_by_region_and_country_WB_merge_cl3.reindex(columns=col_list)
 # Forward fill by row across all columns
 GDP_by_region_and_country_WB_merge_cl4=GDP_by_region_and_country_WB_merge_cl3.ffill(axis=1)
 print(GDP_by_region_and_country_WB_merge_cl4.info())
+print(GDP_by_region_and_country_WB_merge_cl4.head())
 GDP_by_region_and_country_WB_merge_cl4.to_csv("merged_WB_v4.csv")
-
-
-
-# Delete irrelevant rows
-
-# Replace Null values
-
-
-
-# Subset the columns Country Name and Region
+# Set the Indexs to Region and IncomeGroup
+GDP_by_region_and_country_WB_merge_cl4=GDP_by_region_and_country_WB_merge_cl4.set_index("Region","IncomeGroup")
+print(GDP_by_region_and_country_WB_merge_cl4.head())
+# Groupby region and GDP 2019
+GDP_by_region_2000v2019 = GDP_by_region_and_country_WB_merge_cl4.groupby("Region")[["2000","2019"]].sum()
+print(GDP_by_region_2000v2019)
+GDP_by_region_2000v2019.plot(kind="bar", title="GDP by Region 2000 V 2019")
+plt.show()
+GDP_Years=["1960","2000","2019"]
+GDP_by_region = GDP_by_region_and_country_WB_merge_cl4.groupby("Region")[GDP_Years].sum()
+GDP_by_region[GDP_Years].plot(kind="pie", subplots=True, title="GDP by Region 1960, 2000, 2019")
+plt.show()
+# Slice for sub saharan Africa
+sub_saharan_africa_GDP = GDP_by_region_and_country_WB_merge_cl4[GDP_by_region_and_country_WB_merge_cl4["Sub-Saharan Africa"].isin("Region")]
+print(sub_saharan_africa_GDP)
